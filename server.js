eb91643e8f6bb16bcda09aea8f702d30d2d73fb0
piraji <piraji.s@whizit.co.in>
2024-04-06 11:31:12 +0530

    swagger addeed

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+
 connectDb();
 const app = express();
 
@@ -12,6 +16,8 @@ app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
+app.use('/apidoc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
